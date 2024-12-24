482c78a3d2b16739e7567d91c0e74bc7c581dba2
yoann <yoann.sogoyou.pro@gmail.com>
2024-12-24 19:02:44 +0100

    .

---
+++ IBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class IBlock : Block
+    {
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] { new(0,0), new(0,1), new(0,2), new(0,3) },
+            new Position[] { new(0,0), new(1,0), new(2,0), new(3,0) },
+            new Position[] { new(0,0), new(0,1), new(0,2), new(0,3) },
+            new Position[] { new(0,0), new(1,0), new(2,0), new(3,0) }
+        };
+
+        public override int Id => 1;
+        protected override Position StartOffset => new Position(0, 3);
+        protected override Position[][] Tiles => tiles;
+    }
+}
